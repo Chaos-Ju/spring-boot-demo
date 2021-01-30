5df422fb83626b02e964486a84c763836c3edf80
chaosju <chaosju@tencent.com>
2021-01-30 13:42:37 +0800

    增加更详细的注释

--- spring-boot-demo-03-1/src/main/java/com/roncoo/education/controller/IndexController.java
+++ spring-boot-demo-03-1/src/main/java/com/roncoo/education/controller/IndexController.java
@@ -16,19 +16,25 @@ import com.roncoo.education.bean.User;
 
 /**
  * spring-boot-demo-3-1
- * 
+ * 配置文件详解：Properties 学习
+ * 1.配置文件application.properties
+ * 2. 启动时候制定配置文件： java -jar xxxx.jar --spring.config.location=/etc/spring/application.properties
+ * 3.配置文件优先级 ，关注4个 application.properties 的第一行注释，放置的不同位置影响优先级 ，高优先级的配置覆盖低优先级的配置，所有的配置会形成互补配置。
+ * 4.所有配置可以用从命令行制定
+ * 5.配置总体加载规则 由jar包外向jar包内进行寻找，优先加载带profile的，再加载不带profile的。
  * @author wujing
  */
 @RestController
 @RequestMapping(value = "/index")
 public class IndexController {
-	
+
+	//@Value("${roncoo.secret}") 根据application.properties的 key-value直接注入 secret
 	@Value(value = "${roncoo.secret}")
 	private String secret;
-	
+	//@Value("${roncoo.number}") 根据application.properties的 key-value直接注入 id
 	@Value(value = "${roncoo.number}")
 	private int id;
-	
+	//@Value("${roncoo.desc}") 根据application.properties的 key-value直接注入 desc
 	@Value(value = "${roncoo.desc}")
 	private String desc;
 
@@ -39,6 +45,8 @@ public class IndexController {
 
 	// @RequestParam 简单类型的绑定，可以出来get和post
 	@RequestMapping(value = "/get")
+
+	// return ： {"name":"chaosju","secret":"907079a2529927f51fa439063f6d13d5","id":1624519554,"title":"hello world","desc":"the domain is www.roncoo.com"}
 	public HashMap<String, Object> get(@RequestParam String name) {
 		HashMap<String, Object> map = new HashMap<String, Object>();
 		map.put("title", "hello world");
